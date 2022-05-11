561defe66ca5ba6fc81331e6021c3a944d2faad7
김다영 (클라라) <kimdayoung771@gmail.com>
2022-05-11 11:53:39 +0900

    github url changed

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import styles from './footer.module.css'
 const Footer = memo((props) => (
     <footer className={styles.footer}>
         <p className={styles.title}>Code your dream</p>
-        <a href="https://github.com/Dayoung3460/vue-portfolio.git" target="_blank" rel="noreferrer">
+        <a href="https://make-business-card.netlify.app" target="_blank" rel="noreferrer">
             <i className="fab fa-github-square"></i>
         </a>
     </footer>
